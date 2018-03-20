4e1aaa6570041f6db3a7cebb7a6d5835b87e6148
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-03-20 14:07:33 -0700

    Finishied working 1.6 using 'jsx'

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let message = "리액트를 시작해보자"
+    let user = {
+      firstName: "Joungwoo",
+      lastName: "Baik"
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{message + " " + user.firstName + " " + user.lastName}</h2>
       </div>
     );
   }
