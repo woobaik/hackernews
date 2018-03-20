e8d48be5996ea32a6752da5589ecb48fc82cdadc
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-03-20 16:56:26 -0700

    Changed function to ES6 Arrow style

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map(function(item) {
+        {list.map((item) => {
           return (
             <div key={item.objectID}>
               <span>
